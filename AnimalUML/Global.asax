5f0175b1e3ccb65a774f2b95928758980d30153d
Sulman <sulman.malik@hotmail.co.uk>
2019-03-27 19:02:06 +0000

    Initial commit

---
+++ AnimalUML/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AnimalUML.MvcApplication" Language="C#" %>
---
+++ AnimalUML/Models/Animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AnimalUML.Models
+{
+    public class Animal
+    {
+        private string animalName;
+        private int population;
+        private int age;
+        private AnimalType animalType;
+        private ClassifiedAs classifiedAs;
+        private FoodType foodType;
+        private AnimalKingdom animalkingdom;
+
+        public string AnimalName { get => animalName; set => animalName = value; }
+        public int Population { get => population; set => population = value; }
+        public int Age { get => age; set => age = value; }
+        public ClassifiedAs ClassifiedAs { get => classifiedAs; set => classifiedAs = value; }
+        public FoodType FoodType { get => foodType; set => foodType = value; }
+        public AnimalKingdom Animalkingdom { get => animalkingdom; set => animalkingdom = value; }
+    }
+}
